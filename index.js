9d6ace75a7bde9761e24816fada5d66b6db4fc91
anjalydas <164157759+anjalydas@users.noreply.github.com>
2024-09-18 19:14:09 +0530

    committed

--- index.js
+++ index.js
@@ -8,26 +8,10 @@ const apiRouter = require('./routes/index.js');
 const app = express()
 const port = process.env.PORT ||3000;
 connectDB();
-const allowedOrigins = [
-  'http://localhost:5173',
-  'https://capstone-project-online-food-delivery-yummy-client-bx55x1x53.vercel.app/'
-];
-
-// CORS options configuration
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin) || !origin) {
-      // Allow requests with no origin (like mobile apps or curl requests)
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true
-};
-app.use(cors(corsOptions));
-
-
+app.use(cors(
+  {credentials: true,
+  origin: true}
+))
 app.use(express.json())
 app.use(cookieParser());
 app.get("/", (req, res, next) => {
