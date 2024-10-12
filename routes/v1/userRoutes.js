a76f2f226bdc889d22fbe1f4c8070ac3d74677d1
anjalydas <164157759+anjalydas@users.noreply.github.com>
2024-10-12 13:23:57 +0530

    Update userRoutes.js

--- routes/v1/userRoutes.js
+++ routes/v1/userRoutes.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const { getAllUsers, getAUserById, addUser, updateAUserById, deleteAUserById, userLogin, userLogout, userProfile, checkUser } = require('../../controllers/userControllers.js');
+const { getAllUsers, getAUserById, addUser, updateAUserById, deleteAUserById, userLogin, userLogout, userProfile } = require('../../controllers/userControllers.js');
 const authUser = require('../middlewares/authUser.js');
 const authAdmin = require('../middlewares/authAdmin.js');
-const authStoreVender = require('../middlewares/authStoreVender.js');
 const userRouter = express.Router();
 
 
 
 userRouter.get('/', getAllUsers)
-userRouter.get('/:id', getAUserById)
+userRouter.get('/:id', authUser, getAUserById)
 userRouter.post('/:sign-up', addUser)
 userRouter.post('/:login', userLogin)
-userRouter.get('/:logout', authUser, userLogout)
+userRouter.post('/:logout', authUser, userLogout)
 userRouter.post('/:profile/:id', userProfile, authUser)
-userRouter.get('/:id', checkUser, authUser, authAdmin, authStoreVender)
 userRouter.patch('/:id', updateAUserById, authAdmin, authUser)
 userRouter.delete('/:id', deleteAUserById, authAdmin, authUser)
 module.exports = userRouter
