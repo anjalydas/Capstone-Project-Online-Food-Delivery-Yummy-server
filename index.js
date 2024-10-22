a0a1f4595835df9b6705d92d28ab01618f06220b
anjalydas <164157759+anjalydas@users.noreply.github.com>
2024-10-22 13:24:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,13 @@ const apiRouter = require('./routes/index.js');
 const app = express()
 const port = process.env.PORT ; 
 connectDB();
-const allowedOrigins = [
-    'https://capstone-project-online-food-delivery-yummy-client.vercel.app',
-    'http://localhost:3000', // Include localhost for development
-];
+
 app.use(cors({
-  origin: allowedOrigins, 
+  origin: true, 
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   credentials: true,
 }));
+
 
 app.use(express.json())
 app.use(cookieParser());
