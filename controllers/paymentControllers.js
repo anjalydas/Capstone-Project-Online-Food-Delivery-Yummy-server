cbdfdea5b14c7b9f967b4dc3df1b86b840b8032b
anjalydas <164157759+anjalydas@users.noreply.github.com>
2024-10-22 13:29:02 +0530

    Update paymentControllers.js

--- controllers/paymentControllers.js
+++ controllers/paymentControllers.js
@@ -20,9 +20,7 @@ const createPayment = async (req, res, next) => {
             },
             quantity: item.quantity || 1,
         })),
-        mode: "payment",
-        success_url: `${process.env.client_domain}/success`,
-        cancel_url: `${process.env.client_domain}/cancel`,
+        
         metadata: {
             userId: req.userId // Add userId to metadata if available
         },
